6b9f3b5756cb392c9917da28d9a84fc14e2f0291
Jeferson Figueiredo <jefersonlrf@gmail.com>
2026-05-19 05:21:36 -0300

    feat: create city controller and insert cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './user/interfaces/user.entity';
+import { UserEntity } from './user/entities/user.entity';
+import { StateModule } from './state/state.module';
+import { CityModule } from './city/city.module';
+import { AddressModule } from './address/address.module';
 
 @Module({
   imports: [
@@ -21,6 +24,9 @@ import { UserEntity } from './user/interfaces/user.entity';
       migrationsRun: true,
     }),
     UserModule,
+    StateModule,
+    CityModule,
+    AddressModule,
   ],
   controllers: [],
   providers: [],
