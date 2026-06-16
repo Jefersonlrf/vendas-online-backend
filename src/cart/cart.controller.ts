ca23c1dccead1c995d6af463a2106cb7cdd2aef6
Jeferson Figueiredo <jefersonlrf@gmail.com>
2026-06-16 06:27:04 -0300

    feat: create cart of user

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,7 +1,25 @@
 import { Roles } from '@/decorators/roles.decorator';
 import { UserType } from '@/user/enum/user-type.enum';
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { InsertCartDTO } from './dtos/insert-cart.dto';
+import { CartEntity } from './entities/cart.entity';
+import { CartService } from './cart.service';
+import { UserId } from '@/decorators/user-id.decorator';
 
-@Roles(UserType.User)
+@Roles(UserType.User, UserType.Admin)
 @Controller('cart')
-export class CartController {}
+export class CartController {
+
+    constructor(
+        private readonly cartService: CartService
+    ) { }
+
+    @UsePipes(ValidationPipe)
+    @Post()
+    async createCart(
+        @Body() insertCart: InsertCartDTO,
+        @UserId() userId: number,
+    ): Promise<CartEntity | null> {
+        return this.cartService.insertProductInCart(insertCart, userId);
+    }
+}
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,13 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CartEntity } from './entities/cart.entity';
 import { Repository } from 'typeorm';
+import { InsertCartDTO } from './dtos/insert-cart.dto';
 
 @Injectable()
 export class CartService {
 
     constructor(
         @InjectRepository(CartEntity)
-        private readonly cartRepository: Repository<CartEntity>   
-    ){}
+        private readonly cartRepository: Repository<CartEntity>,
+    ) { }
+
+    async verifyActiveCart(userId: number): Promise<CartEntity | null> {
+        const cart = await this.cartRepository.findOne({
+            where: {
+                userId,
+            },
+        });
+
+        if (!cart) {
+            throw new NotFoundException('Cart active not found');
+        }
+        return cart;
+    }
+
+    async createCard(userId: number): Promise<CartEntity> {
+        return await this.cartRepository.save({
+            active: true,
+            userId,
+        });
+
+    }
+
+    async insertProductInCart(insertCart: InsertCartDTO, userId: number): Promise<CartEntity | null> {
+        const cart = await this.verifyActiveCart(userId).catch(async () => {
+            return this.createCard(userId);
+        });
+        return cart;
+    }
 }
