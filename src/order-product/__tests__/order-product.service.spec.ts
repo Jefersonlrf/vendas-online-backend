757ca44410ad8bdf896821b243afba3b7a9a56ac
Jeferson Figueiredo <jefersonlrf@gmail.com>
2026-08-22 19:36:30 -0300

    feat: start unit test in payment

--- src/order-product/__tests__/order-product.service.spec.ts
+++ src/order-product/__tests__/order-product.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { OrderProductService } from './order-product.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { OrderProductEntity } from '../entities/order-product.entity';
+import { OrderProductService } from '../order-product.service';
 
 describe('OrderProductService', () => {
   let service: OrderProductService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrderProductService],
+      providers: [
+        OrderProductService,
+        {
+          provide: getRepositoryToken(OrderProductEntity),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<OrderProductService>(OrderProductService);
--- src/order/__tests__/order.service.spec.ts
+++ src/order/__tests__/order.service.spec.ts
@@ -1,3 +1,7 @@
+import { CartService } from '@/cart/cart.service';
+import { OrderProductService } from '@/order-product/order-product.service';
+import { PaymentService } from '@/payment/payment.service';
+import { ProductService } from '@/product/product.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -7,11 +11,40 @@ import { OrderService } from '../order.service';
 describe('OrderService', () => {
   let service: OrderService;
   let orderRepository: Repository<OrderEntity>;
+  let paymentService: PaymentService;
+  let cartService: CartService;
+  let orderProductService: OrderProductService;
+  let productService: ProductService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         OrderService,
+        {
+          provide: PaymentService,
+          useValue: {
+            createPayment: jest.fn(),
+          },
+        },
+        {
+          provide: CartService,
+          useValue: {
+            findCartByUserId: jest.fn(),
+            clearCart: jest.fn(),
+          },
+        },
+        {
+          provide: OrderProductService,
+          useValue: {
+            createOrderProduct: jest.fn(),
+          },
+        },
+        {
+          provide: ProductService,
+          useValue: {
+            findAll: jest.fn(),
+          },
+        },
         {
           provide: getRepositoryToken(OrderEntity),
           useValue: {
@@ -22,6 +55,10 @@ describe('OrderService', () => {
     }).compile();
 
     service = module.get<OrderService>(OrderService);
+    paymentService = module.get<PaymentService>(PaymentService);
+    cartService = module.get<CartService>(CartService);
+    orderProductService = module.get<OrderProductService>(OrderProductService);
+    productService = module.get<ProductService>(ProductService);
     orderRepository = module.get<Repository<OrderEntity>>(
       getRepositoryToken(OrderEntity),
     );
@@ -30,5 +67,9 @@ describe('OrderService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
     expect(orderRepository).toBeDefined();
+    expect(cartService).toBeDefined();
+    expect(paymentService).toBeDefined();
+    expect(orderProductService).toBeDefined();
+    expect(productService).toBeDefined();
   });
 });
